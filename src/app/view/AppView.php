f613ca7a73abfa15c1b8db250bbc078ef39882dd
Mathias VINCENT <vmathias.fr@gmail.com>
2017-03-30 07:49:10 +0200

    Une argile au creux de mes mains

--- src/app/view/AppView.php
+++ src/app/view/AppView.php
@@ -13,7 +13,21 @@ class AppView
 {
 
     function index() {
-        echo '<h1>Application BD de VINCENT Mathias, SIPP hugo, WOLF Vianney et BECKER Yann</h1>';
+        $html = <<<END
+        <h1>Application BD de VINCENT Mathias, SIPP hugo, WOLF Vianney et BECKER Yann</h1>
+        <h2>TD1</h2>
+        <ul>
+            <li><a href='jeux/mario'>Q1</a></li>
+            <li><a href='company/japon'>Q2</a></li>
+            <li><a href='jeux/plateform/dixmillek'>Q3</a></li>
+            <li><a href='jeux/listJeux'>Q4</a></li>
+            <li><a href='jeux/listByPage/1'>Q5</a></li>
+        </ul>
+               
+END;
+
+        echo $html;
+
 
     }
 
